424de49719ed28245dba785e42009a8a788b47b3
iicefy <chawit_rittiya@hotmail.com>
2025-05-08 21:43:41 +0700

    Add comments to clarify mutable variable usage in show_variables function

--- src/variables.rs
+++ src/variables.rs
@@ -1,4 +1,5 @@
 pub fn show_variables() {
+    // mut for mutable variable
     let mut name = "Ice";
     let age = 28;
     let is_active = true;
@@ -7,6 +8,7 @@ pub fn show_variables() {
     println!("Age: {}", age);
     println!("Active: {}", is_active);
 
+    // reassign mutable variable
     name = "Chawit";
 
     println!("Edited Name: {}", name);
